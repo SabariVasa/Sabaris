a8f5bc3f7031f7484cab92639bdca7b524b9b039
balasubramani.a <balasubramani.a@hrlytics.in>
2024-09-10 23:03:51 +0530

    initial commit

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import LaptopImg from "../../assets/photo.jpg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+const About = () => {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Hi, my name is <span className="yellow">Balasubramani A </span>
+              and I'm from <span className="yellow"> Ramanathapuram, TamilNadu.</span>
+              <br />
+              <br />
+              I recently graduated with a Bachelor's degree in Electrical And Electronics and Engineering in 2022.
+              <br />
+              <br />
+              As a
+              <b className="yellow"> Front-End </b>developer,
+              I enjoy tackling new challenges and continuously expanding my skillset.
+              <br />
+              <br />I am proficient in
+              <b className="yellow"> Javascript, </b>
+              as well as have knowledge in programming languages such as C, Python
+              <br />
+              <br />
+              I have a passion for working
+              with <b className="yellow">Node.js, MongoDB,</b> and
+              <i>
+                <b className="yellow">
+                  {" "}
+                  modern Javascript libraries and frameworks
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="yellow"> React.js</b>
+              </i>
+              <br />
+              <br />
+              I am also interested in building new
+              <i>
+                <b className="yellow"> Web Technologies and Products. </b>
+                {/* <b className="yellow"> Artificial Intelligence.</b> */}
+              </i>
+              <br />
+
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={LaptopImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/balasubramaniaji01"
+                  style={{ color: "white" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="github"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/balasubramani-a-7807b4243/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="linkedin"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+
+}
+
+export default About
